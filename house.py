19ac9daa923993da07f670204cb3bb064fe9a13d
Oguz Enver Uzan <158345675+oguzuzan@users.noreply.github.com>
2024-03-08 00:46:56 +0300

    Update house.py

--- house.py
+++ house.py
@@ -144,15 +144,15 @@ from sklearn.preprocessing import LabelEncoder
 import joblib
 
 # Kaydettigim Joblib dosyalarini Cagirmak icin
-# Önbellek kullanımı
-@st.cache_data(persist=True)
 def load_model_and_features():
+    # Modeli cagiriyorum.
     model = joblib.load('new_model_xgb.joblib')
+
+    # Sutun isimlerini cagiriyorum.
     feature_names = joblib.load('feature_names.joblib')
-    
+
     return model, feature_names
 
-@st.cache_data(hash_funcs={xgb.sklearn.XGBRegressor: id})
 def predict_sale_price(model, input_data):
     label_encoder = LabelEncoder()
 
@@ -164,9 +164,7 @@ def predict_sale_price(model, input_data):
     return np.expm1(prediction)  # Reverse the log transformation
 
 # Sutunlarin Aciklama Kismi icin
-@st.cache_data(persist=True)
-def get_feature_descriptions():
-    return {
+feature_descriptions = {
     'OverallQual': 'Genel malzeme ve kaplama kalitesi',
     'GarageType_Attchd': 'Ek garaj Var ise 1 Yok ise 0',
     'BsmtQual': 'Bodrum katının yüksekliği (Yok ise 0)',
@@ -181,7 +179,6 @@ def get_feature_descriptions():
     'CentralAir': 'Klima Var mi?',
     'KitchenQual': 'Mutfak kalitesi',
     'MSZoning_RL': 'Ev Az nüfuslu Bir Yerde mi?'
-    
 }
 
 # House App
@@ -196,37 +193,29 @@ def main():
 
     # Sutunlarin Ne oldugunu aciklayan SideBar
     st.sidebar.title("Feature Descriptions")
-    feature_descriptions = get_feature_descriptions()
     for feature, description in feature_descriptions.items():
         st.sidebar.markdown(f"**{feature}**: {description}")
 
     # Inputlari girdigimiz yerin Duzenlemesi ve Ayarlanmasi
     input_features = {}
+
+    input_methods = {
+    'OverallQual': lambda: st.slider(f"Select Overall Quality", 1, 10, value=5),
+    'GarageType_Attchd': lambda: st.selectbox("Select GarageType_Attchd", ['0', '1']),
+    'BsmtQual': lambda: st.selectbox("Select BsmtQual", ['0', '1', '2', '3', '4']),
+    'GarageCars': lambda: st.number_input("Enter GarageCars", value=0, step=1),
+    'FullBath': lambda: st.number_input("Enter FullBath", value=0, step=1),
+    'Fireplaces': lambda: st.number_input("Enter Fireplaces", value=0, step=1),
+    'PavedDrive': lambda: st.selectbox("Select Paved Drive", [0, 1]),
+    'ExterQual': lambda: st.slider("Select Exter Quality", 1, 3, value=5),
+    'YearRemodAdd': lambda: st.number_input("Enter YearRemodAdd", value=0, step=1),
+    'CentralAir': lambda: st.selectbox("Select CentralAir", [0, 1]),
+    'KitchenQual': lambda: st.slider("Select KitchenQual", 1, 3, value=5),
+    'MSZoning_RL': lambda: st.selectbox("Select MSZoning_RL", [0, 1]),
+}
+
     for feature in feature_names:
-        if feature == 'OverallQual':
-            input_features[feature] = st.slider(f"Select {feature}", 1, 10, value=5)
-        elif feature == 'GarageType_Attchd':
-            input_features[feature] = st.selectbox(f"Select {feature}", ['0', '1'])
-        elif feature == 'BsmtQual':
-            input_features[feature] = st.selectbox(f"Select {feature}", ['0', '1', '2', '3', '4'])
-        elif feature == 'GarageCars':
-            input_features[feature] = st.number_input(f"Enter {feature}", value=0, step=1)
-        elif feature in ["FullBath", "Fireplaces"]:
-            input_features[feature] = st.number_input(f"Enter {feature}", value=0, step=1)
-        elif feature == 'PavedDrive':
-            input_features[feature] = st.selectbox(f"Select {feature}", [0, 1])
-        elif feature == 'ExterQual':
-            input_features[feature] = st.slider(f"Select {feature}", 1, 3, value=5)
-        elif feature == 'YearRemodAdd':
-            input_features[feature] = st.number_input(f"Enter {feature}", value=0, step=1)
-        elif feature == 'CentralAir':
-            input_features[feature] = st.selectbox(f"Select {feature}", [0, 1])
-        elif feature == 'KitchenQual':
-            input_features[feature] = st.slider(f"Select {feature}", 1, 3, value=5)
-        elif feature == 'MSZoning_RL':
-            input_features[feature] = st.selectbox(f"Select {feature}", [0, 1])
-        else:
-            input_features[feature] = st.number_input(f"Enter {feature}", value=0.0)
+        input_features[feature] = input_methods.get(feature, lambda: st.number_input(f"Enter {feature}", value=0.0))()
 
     # Girdigimiz inputlari kaydeden bir DataFrame Olusturmak icin (Opsiyonel)
     input_df = pd.DataFrame([input_features])
