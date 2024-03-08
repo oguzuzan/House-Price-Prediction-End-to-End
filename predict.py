2d45f68918d4e265c08da20aea813cee129ccbf1
Oguz Enver Uzan <158345675+oguzuzan@users.noreply.github.com>
2024-03-08 17:52:47 +0300

    Update predict.py

--- predict.py
+++ predict.py
@@ -20,23 +20,6 @@ def predict_sale_price(model, input_data):
     prediction = model.predict(input_data)
     return np.expm1(prediction)  # Reverse the log transformation
 
-# Sutunlarin Aciklama Kismi icin
-feature_descriptions = {
-    'OverallQual': 'Genel malzeme ve kaplama kalitesi',
-    'GarageType_Attchd': 'Ek garaj Var ise 1 Yok ise 0',
-    'BsmtQual': 'Bodrum katının yüksekliği (Yok ise 0)',
-    'TotalSF': 'Evin Toplam Metrekaresi',
-    'GarageCars': 'Garaja sığan araba sayısı',
-    'FullBath': 'Banyo Sayısı',
-    'GrLivArea': 'Ust kat var ise Metrekaresi Yok ise 0',
-    'PavedDrive': 'Asfaltlanmış araba yolu var mi? Yoksa 0',
-    'ExterQual': 'Dış malzeme kalitesi',
-    'Fireplaces': 'Şömine sayısı',
-    'YearRemodAdd': 'Tadilat tarihi (tadilat veya ekleme yoksa inşaat tarihiyle aynı)',
-    'CentralAir': 'Klima Var mi?',
-    'KitchenQual': 'Mutfak kalitesi',
-    'MSZoning_RL': 'Ev Az nüfuslu Bir Yerde mi?'
-}
 
 # House App
 def main():
@@ -44,40 +27,125 @@ def main():
 
     # Modeli Cagiriyoruz.
     trained_model = model
+    feature_names = joblib.load('feature_names.joblib') 
 
+    # Resim eklemek için st.image fonksiyonunu kullanın
+    st.image("house.png", use_column_width=True)
     # 2.Baslik
-    st.header("User Input Features")
-
-    # Sutunlarin Ne oldugunu aciklayan SideBar
-    st.sidebar.title("Feature Descriptions")
-    for feature, description in feature_descriptions.items():
-        st.sidebar.markdown(f"**{feature}**: {description}")
+    st.header("Evin Bilgilerini Giriniz.")
 
     # Inputlari girdigimiz yerin Duzenlemesi ve Ayarlanmasi
     input_features = {}
     for feature in feature_names:
         if feature == 'OverallQual':
-            input_features[feature] = st.slider(f"Select {feature}", 1, 10, value=5)
+            input_features[feature] = st.slider("Genel malzeme ve kaplama kalitesi: (1-10)", 1, 10, value=10)
+            puanlar_metin = {
+            10: "Very Excellent",
+            9: "Excellent",
+            8: "Very Good",
+            7: "Good",
+            6: "Above Average",
+            5: "Average",
+            4: "Below Average",
+            3: "Fair",
+            2: "Poor",
+            1: "Very Poor"
+            }
+            secilen_metin = puanlar_metin.get(input_features[feature], "Bilinmeyen Puan")
+            input_features[feature] = input_features[feature]
+            st.write(f"Seçilen Puan: {input_features[feature]} - {secilen_metin}")
+
         elif feature == 'GarageType_Attchd':
-            input_features[feature] = st.selectbox(f"Select {feature}", ['0', '1'])
+            input_features[feature] = st.selectbox("Ek garaj Var mı?", {'Hayir': 0, 'Evet': 1}, key="garaj_selectbox")
         elif feature == 'BsmtQual':
-            input_features[feature] = st.selectbox(f"Select {feature}", ['0', '1', '2', '3', '4'])
+            bsmt_qual_siniflandirma = {
+                '5': "Excellent (100+ inches)",
+                '4': "Good (90-99 inches)",
+                '3': "Typical (80-89 inches)",
+                '2': "Fair (70-79 inches)",
+                '1': "Poor (<70 inches)",
+                '0': "No Basement",
+            }
+            # Benzersiz bir key değeri oluşturunuz (örneğin, feature adını kullanabilirsiniz)
+            selectbox_key = f"{feature}_selectbox"
+            # Dış malzeme kalitesi için Streamlit selectbox'ını kullanarak değeri alınız
+            input_features[feature] = st.selectbox("Bodrum Katının Kalitesini Seçin:", list(bsmt_qual_siniflandirma.keys()), key=selectbox_key)
+
+            # Kullanıcının seçtiği değere göre sınıflandırmayı belirleyiniz
+            sınıflandırma = bsmt_qual_siniflandirma[input_features[feature]]
+
+            # Sınıflandırmayı ve seçilen değeri ekrana yazdırınız
+            st.write(f"Seçilen: {input_features[feature]} - Sınıflandırma: {sınıflandırma}")
         elif feature == 'GarageCars':
-            input_features[feature] = st.number_input(f"Enter {feature}", value=0, step=1)
-        elif feature in ["FullBath", "Fireplaces"]:
-            input_features[feature] = st.number_input(f"Enter {feature}", value=0, step=1)
+            input_features[feature] = st.number_input("Garaja sığan araba sayısı", value=0, step=1)
+        elif feature in ["FullBath"]:
+            input_features[feature] = st.number_input("Banyo Sayısı", value=0, step=1)
+        elif feature == 'Fireplaces':
+            input_features[feature] = st.number_input("Şömine sayısı", value=0, step=1)
         elif feature == 'PavedDrive':
-            input_features[feature] = st.selectbox(f"Select {feature}", [0, 1])
+            paved_road_description = {
+                'Asfaltlı Yol': '2',
+                'Yarı Asfaltlı': '1',
+                'Toprak Yol': '0'
+            }
+            input_features[feature] = st.selectbox("Asfaltlanmış araba yolu var mi?", list(paved_road_description.keys()))
+        
         elif feature == 'ExterQual':
-            input_features[feature] = st.slider(f"Select {feature}", 1, 3, value=5)
+            puanlar = {
+                    5: "Ex",
+                    4: "Gd",
+                    3: "TA",
+                    2: "Fa",
+                    1: "Po",
+            }
+            # Sınıflandırma sözlüğü
+            siniflandirma = {
+                5: "Excellent",
+                4: "Good",
+                3: "Average/Typical",
+                2: "Fair",
+                1: "Poor",
+            }
+            # Benzersiz bir key değeri oluşturunuz (örneğin, feature adını kullanabilirsiniz)
+            slider_key = f"{feature}_slider"
+            # Dış malzeme kalitesi için Streamlit slider'ını kullanarak değeri alınız
+            input_features[feature] = st.slider("Dış malzeme kalitesi", 1, 5, value=5, key=slider_key)
+
+            # Kullanıcının seçtiği değere göre sınıflandırmayı belirleyiniz
+            sınıflandırma = siniflandirma[input_features[feature]]
+
+            # Sınıflandırmayı ekrana yazdırınız
+            st.write(f"Seçilen {input_features[feature]} :", sınıflandırma)
+
         elif feature == 'YearRemodAdd':
-            input_features[feature] = st.number_input(f"Enter {feature}", value=0, step=1)
+            input_features[feature] = st.number_input("Tadilat tarihi (tadilat veya ekleme yoksa inşaat tarihiyle aynı)", value=0, step=1)
         elif feature == 'CentralAir':
-            input_features[feature] = st.selectbox(f"Select {feature}", [0, 1])
+            input_features[feature] = st.selectbox("Klima Var mi?", {'Hayır': 0, 'Evet': 1})
         elif feature == 'KitchenQual':
-            input_features[feature] = st.slider(f"Select {feature}", 1, 3, value=5)
+            # Sınıflandırma sözlüğü
+            siniflandirma_kitchen = {
+                5: "Excellent",
+                4: "Good",
+                3: "Average/Typical",
+                2: "Fair",
+                1: "Poor",
+            }
+            # Benzersiz bir key değeri oluşturunuz (örneğin, feature adını kullanabilirsiniz)
+            slider_key = f"{feature}_slider"
+            # Dış malzeme kalitesi için Streamlit slider'ını kullanarak değeri alınız
+            input_features[feature] = st.slider("Mutfak Kalitesi", 1, 5, value=5, key=slider_key)
+
+            # Kullanıcının seçtiği değere göre sınıflandırmayı belirleyiniz
+            siniflandirma_kitchen = siniflandirma_kitchen[input_features[feature]]
+
+            # Sınıflandırmayı ekrana yazdırınız
+            st.write(f"Seçilen {input_features[feature]} :", siniflandirma_kitchen)
         elif feature == 'MSZoning_RL':
-            input_features[feature] = st.selectbox(f"Select {feature}", [0, 1])
+            input_features[feature] = st.selectbox("Ev Az nüfuslu Bir Yerde mi?", {'Hayir': 0, 'Evet': 1})
+        elif feature == 'TotalSF':
+            input_features[feature] = st.number_input("Evin Toplam Metrekaresi", value=0.0)
+        elif feature == 'GrLivArea':
+            input_features[feature] = st.number_input("Zemin Katın Metrekaresi", value=0.0)
         else:
             input_features[feature] = st.number_input(f"Enter {feature}", value=0.0)
 
